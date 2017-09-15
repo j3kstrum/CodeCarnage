be77baf2c79409030f9a4e790c45b705df0381ca
Jacob Ekstrum <jacobeks@buffalo.edu>
2017-09-15 17:56:41 -0400

    Add VASTLY superior ticking functionality. Also fill out some nested file structures and added some stubs for other sections of the code.

--- src/main/java/common/Main.java
+++ src/main/java/common/Main.java
@@ -14,16 +14,17 @@ public class Main {
 
     private static Engine _ENGINE;
     private static GUI _GUI;
+    private static final BaseLogger LOGGER = new BaseLogger("Main");
 
     /**
      * GUI method for the project. Responsible for initializing all core modules.
      * @param args The system's command line arguments. This should be empty.
      */
     public static void main(String[] args) {
-        new Thread(
-                () -> _GUI = new GUI(args)
-        ).start();
+        _GUI = new GUI(args);
         _ENGINE = new Engine();
+        LOGGER.debug("GUI and Engine created. Setting observer...");
+        _GUI.observe(_ENGINE);
     }
 
 }
