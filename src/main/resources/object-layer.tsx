cdcb715defc365c5ba715cc40116a492f409cf83
Nick Martin <mcnartin5@gmail.com>
2017-09-20 14:02:24 -0400

    Add tsx files. closes #43 closes #44

--- src/main/resources/object-layer.tsx
+++ src/main/resources/object-layer.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="object- layer" tilewidth="32" tileheight="32" tilecount="84" columns="14">
- <image source="../../Downloads/object- layer.png" width="477" height="219"/>
+ <image source="./object- layer.png" width="477" height="219"/>
 </tileset>
