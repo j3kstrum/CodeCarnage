c8d6b81f80b069580862bc42dd3ebe21b4e6fd50
Nick Martin <mcnartin5@gmail.com>
2017-09-22 20:04:51 -0400

    Create a new game window from menu. Closes #48

--- src/main/java/gui/game/GameGUI.java
+++ src/main/java/gui/game/GameGUI.java
@@ -3,9 +3,11 @@ package gui.game;
 import common.BaseLogger;
 import engine.core.Engine;
 import javafx.application.Application;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 
@@ -17,12 +19,12 @@ public class GameGUI extends Application {
     public GameGUI() throws Exception {
         new Thread().start();
 
-        start(new Stage());
-
         //Create Engine
         _engine = new Engine();
         LOGGER.info("Beginning core game battle...");
         this._engine.startGame();
+
+        start(new Stage());
     }
 
     public GameGUI(String[] args) {
@@ -35,6 +37,11 @@ public class GameGUI extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("game.fxml"));
         Parent root = loader.load();
+
+        ObservableMap map = loader.getNamespace();
+
+        ImageView i = (ImageView) map.get("viewer");
+
         primaryStage.setTitle("Code Carnage");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
---
+++ src/main/java/gui/game/MapRender.java
@@ -0,0 +1,12 @@
+package gui.game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MapRender extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
