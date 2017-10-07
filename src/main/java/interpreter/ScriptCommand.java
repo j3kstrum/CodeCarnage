7073e2c0a6ec2bcc21ab7bd1f2e9ad61a28c649c
David Olsen <davidols@buffalo.edu>
2017-10-07 17:34:55 -0400

    Started implementing the ScriptCommand class.

--- src/main/java/interpreter/ScriptCommand.java
+++ src/main/java/interpreter/ScriptCommand.java
@@ -2,6 +2,7 @@ package interpreter;
 
 import interpreter.enumerations.Command;
 
+import utilties.models.Game;
 import java.util.List;
 
 /**
@@ -11,10 +12,14 @@ public class ScriptCommand {
 
     private List<Check> checks;
     private Command command;
+    private int id;
+    private int otherId;
 
-    public ScriptCommand(List<Check> checks, Command command) {
+    public ScriptCommand(List<Check> checks, Command command, int id) {
         this.checks = checks;
         this.command = command;
+        this.id = id;
+        if (id == 1){this.otherId = 0;} else {this.otherId = 1;};
     }
 
     public Command getCommand() {
@@ -23,5 +28,36 @@ public class ScriptCommand {
 
     public List<Check> getChecks() {
         return checks;
+    }
+
+    public boolean performCommand(Game game){
+
+        for (Check c: this.checks){
+            if (!c.conditionIsTrue(game)){
+                return false;
+            }
+        }
+
+        doCommand(game);
+        return true;
+    }
+
+
+    private void doCommand(Game game){
+
+        switch(this.command){
+            case APPROACH:
+                game.approach(this.id, this.otherId);
+                break;
+            case HEAL:
+                game.heal(this.id, 20);
+                break;
+            case DO_NOTHING:
+                game.doNothing(this.id);
+                break;
+            case DEFEND:
+                game.defend(this.id);
+                break;
+        }
     }
 }
