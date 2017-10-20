f2c6039c0db7837f2d9c7cef2db35521f8e59ad2
Nick Martin <mcnartin5@gmail.com>
2017-10-19 21:23:33 -0400

    Removed clutter from initial attempt. Closes #147

--- src/main/java/gui/scripting/ScriptingGUI.java
+++ src/main/java/gui/scripting/ScriptingGUI.java
@@ -7,16 +7,10 @@
 
 package gui.scripting;
 
-import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
-import javafx.collections.ObservableMap;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -46,50 +40,17 @@ public class ScriptingGUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("scripting.fxml"));
-        ObservableMap nodesMap = loader.getNamespace();
-
-        //Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-
 
         Parent root = loader.load();
 
         primaryStage.setTitle("Code Carnage");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
-        //primaryStage.setWidth(visualBounds.getWidth());
-        //primaryStage.setHeight(visualBounds.getHeight());
-        //setDimensions(nodesMap, visualBounds);
         primaryStage.show();
 
         // Assign the stylesheet to the Scene
         root.getStylesheets().add(
                 getClass().getResource("/styles/script.css").toExternalForm()
         );
-    }
-
-    private void setDimensions(ObservableMap map, Rectangle2D visualBounds) {
-        ScrollPane choicesScroll = (ScrollPane) map.get("choicesScroll");
-        ScrollPane behaviorsScroll = (ScrollPane) map.get("behaviorsScroll");
-        AnchorPane behaviorPane = (AnchorPane) map.get("behaviorPane");
-        BehaviorList behaviorList = (BehaviorList) map.get("behaviorList");
-        JFXButton add = (JFXButton) map.get("add");
-        JFXButton submit = (JFXButton) map.get("submit");
-        JFXButton subtract = (JFXButton) map.get("subtract");
-        JFXButton deleteWord = (JFXButton) map.get("deleteWord");
-        Pane logoPane = (Pane) map.get("logoPane");
-
-
-        behaviorsScroll.setPrefWidth(.75 * visualBounds.getWidth());
-        behaviorsScroll.setPrefHeight(.5 * visualBounds.getHeight());
-        behaviorPane.setPrefWidth(.75 * visualBounds.getWidth());
-        behaviorPane.setPrefHeight(.75 * visualBounds.getHeight());
-
-        behaviorsScroll.setFitToWidth(true);
-        behaviorsScroll.setFitToHeight(true);
-        behaviorList.setPrefWidth(.73 * visualBounds.getWidth());
-
-        choicesScroll.setPrefHeight(.9 * visualBounds.getHeight());
-
-
     }
 }
